fb6cbd1fdf05d212a7840fe9ee8130a64860044a
Heng Xiao <heng.xiao@pnnl.gov>
2024-01-30 16:52:50 -0500

    plot the marker for ENA correctly now.

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 from plot_goesr import download_daily_data
 from datetime import datetime
 
-download_daily_data(2018, 2, 7)
+download_daily_data(2018, 10, 28)
 
 
--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 from plot_goesr import make_goes16_ena_daily_movie, make_goes16_ena_daily_plots, download_daily_data
 
-year, month, day = 2018, 2, 12 
+year, month, day = 2018, 11, 21 
 download_daily_data(year, month, day)
 make_goes16_ena_daily_plots(year, month, day)
 make_goes16_ena_daily_movie(year, month, day)
